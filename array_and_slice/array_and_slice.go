3e2e6a3d1797bbc6ce1c6cb5f5b0d6d8f97b769c
jhaymesdev <jhaymescode@gmail.com>
2022-10-24 00:14:51 +0100

    array and slice

---
+++ array_and_slice/array_and_slice.go
@@ -0,0 +1,53 @@
+package list
+
+
+
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+// func Sum(numList [5]int) int {
+// 	defaultNumber := 0
+
+// 	for i := 0; i< 5; i++{
+//         defaultNumber+= numList[i]                          
+// 	}
+	
+// 	return defaultNumber;
+// }
+
+// func SumAll(numbersToSum ...[]int) []int {
+// 	lengthOfNumbers := len(numbersToSum)
+// 	sums := make([]int, lengthOfNumbers)
+
+// 	for i, numbers := range numbersToSum {
+// 		sums[i] = Sum(numbers)
+// 	}
+
+// 	return sums
+// }
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
